7a960ae6da2f5900232cd504b39eed7d5a329128
Tarun Gehlaut <tarungerhlaut330@gmail.com>
2023-06-15 16:08:05 +0530

    removed Strict Mode

--- src/index.js
+++ src/index.js
@@ -5,11 +5,7 @@ import "./index.css";
 
 try {
   const root = createRoot(document.getElementById("root"));
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+  root.render(<App />);
 } catch (error) {
   console.error(error);
 }
